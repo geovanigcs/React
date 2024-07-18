6aae19dcf2442065a5e904680e6ea3f706edc97d
Geovani Cordeiro <geovanicordeiro13@gmail.com>
2024-07-18 11:43:02 -0300

    maps

---
+++ project/03/index.js
@@ -0,0 +1,43 @@
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com "a"
+
+
+// const pessoas = [
+//     { nome: 'Luiz', idade: 62},
+//     { nome: 'Maria', idade: 23},
+//     { nome: 'Eduardo', idade: 55},
+//     { nome: 'Letícia', idade: 19},
+//     { nome: 'Rosana', idade: 32},
+//     { nome: 'Wallace', idade: 47},
+// ]
+
+// const nomes5 = pessoas.filter(pessoa => pessoa.nome.length >= 5)
+// console.log(nomes5)
+
+// const idade50 =  pessoas.filter(pessoa => pessoa.idade > 50)
+// console.log(idade50)
+
+// const nomend = pessoas.filter(pessoa => pessoa.nome.endsWith('a'))
+// console.log(nomend)
+
+// Dobre os números
+
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+// const dobrar = numeros.map(dobro => dobro*2)
+// console.log(dobrar)
+
+//Para cada elemento
+// Retorne apenas uma string com o nome da pessoa
+// Remova apenas a chave "nome" do objeto
+// Adicione uma chave id em cada objeto
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62},
+    { nome: 'Maria', idade: 23},
+    { nome: 'Eduardo', idade: 55},
+    { nome: 'Letícia', idade: 19},
+    { nome: 'Rosana', idade: 32},
+    { nome: 'Wallace', idade: 47},
+]
