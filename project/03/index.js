6b903ab6cde53fc660afa3a912a33888e7fbac05
Geovani Cordeiro <geovanicordeiro13@gmail.com>
2024-07-23 11:29:35 -0300

    filter, map and reduce

--- project/03/index.js
+++ project/03/index.js
@@ -1,6 +1,6 @@
-Retorne as pessoas que tem o nome com 5 letras ou mais
-Retorne as pessoas com mais de 50 anos
-Retorne as pessoas cujo nome termina com "a"
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com "a"
 
 
 const pessoas = [
@@ -13,25 +13,21 @@ const pessoas = [
 ]
 
 const nomes5 = pessoas.filter(pessoa => pessoa.nome.length >= 5)
-console.log(nomes5)
 
 const idade50 =  pessoas.filter(pessoa => pessoa.idade > 50)
-console.log(idade50)
 
 const nomend = pessoas.filter(pessoa => pessoa.nome.endsWith('a'))
-console.log(nomend)
 
-Dobre os números
+// Dobre os números
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
 const dobrar = numeros.map(dobro => dobro*2)
-console.log(dobrar)
 
-Para cada elemento
-Retorne apenas uma string com o nome da pessoa
-Remova apenas a chave "nome" do objeto
-Adicione uma chave id em cada objeto
+// Para cada elemento
+// Retorne apenas uma string com o nome da pessoa
+// Remova apenas a chave "nome" do objeto
+// Adicione uma chave id em cada objeto
 
 const pessoas = [
     { nome: 'Luiz', idade: 62},
@@ -47,30 +43,54 @@ const nonames = pessoas.map(pessoa => (delete pessoa.nome))
 const ids = pessoas.map((pessoa, indice) => (pessoa.id = indice + 1))
 
 
-Some todos os números (reduce)
-Retorne um array com os pares (Filter)
-Retorne um array com com o dobro dos valores (Map)
+// Some todos os números (reduce)
+// Retorne um array com os pares (Filter)
+// Retorne um array com com o dobro dos valores (Map)
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
-Retorne a pessoa mais velha
+// Retorne a pessoa mais velha
 
 const pessoas = [
     { nome: 'Luiz', idade: 62},
     { nome: 'Maria', idade: 23},
     { nome: 'Eduardo', idade: 55},
-    { nome: 'Letícia', idade: 19},
-    { nome: 'Rosana', idade: 32},
-    { nome: 'Wallace', idade: 47},
+    { nome: 'Letícia', idade: 10},
+    { nome: 'Rosana', idade: 64},
+    { nome: 'Wallace', idade: 63},
 ]
 
-const soma = numeros.reduce((valor, acumulador) => acumulador += valor)
-console.log(soma)
+const soma = numeros.reduce((valor, acumulador) => acumulador + valor)
 
 
-const pares = numeros.reduce((acumulador, valor) => {if (valor % 2 === 0) acumulador += valor})
-console.log(pares)
+const pares = numeros.reduce((acumulador, valor) =>  valor % 2 === 0 ? [...acumulador, valor] : acumulador, []) //Aqui está sendo feito com reduce
+
+const pares = numeros.filter(valor => valor % 2 === 0) // Com filtro é muito mais facil
 
 
-const pares = numeros.filter(pares => pares.numeros.endsWith('5'))
-console.log(pares)
+const dobrar = numeros.map(valor => valor * 2)
+
+const old = pessoas.reduce((acumulador, valor) => (acumulador.idade > valor.idade ? acumulador : valor), pessoas[0]) 
+
+const pessoas = [
+    { nome: 'João', idade: 25 },
+    { nome: 'Maria', idade: 30 },
+    { nome: 'José', idade: 45 },
+    { nome: 'Ana', idade: 20 }
+  ]
+
+const old1 = pessoas.reduce( (acumulador, valor) => (acumulador.idade > valor.idade ? acumulador : valor), pessoas[0])
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const pares1 = numeros.filter( valor => valor % 2 === 0)
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const soma1 = numeros.map( valor => valor + valor)
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const dobrar1 = numeros.map( valor => valor * 2)
+
+const frutas = ['maçã', 'banana', 'maçã', 'laranja', 'banana', 'maçã']
+const quantidade = frutas.reduce((acumulador, valor) => ({...acumulador,[valor] : (acumulador[valor] || 0) + 1}),{})
+console.log(quantidade)
+
